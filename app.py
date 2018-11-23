62a0ba229ad209840937242031a8be0749f48182
Stéphane Schenkel <stephane.schenkel@etu.u-pec.fr>
2018-11-23 18:44:50 +0100

    modify ip to backend

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import requests
 
-resp = requests.get("http://10.10.10.130:8090/poulet")
+resp = requests.get("http://backend:8090/poulet")
 if resp.status_code != 200:
     raise Exception("GET /poulet {}".format(resp.status_code))
 print(resp.json())
 
-resp = requests.post("http://10.10.10.130:8090/poulet", json={'type': 'braise'})
+resp = requests.post("http://backend:8090/poulet", json={'type': 'braise'})
 if resp.status_code != 200:
     raise Exception("POST /poulet {}".format(resp.status_code))
 print(resp.json())
